3ac750dabb6572acc37381bbb6b19df4f3f021c1
loganmca01 <loganmca01@gmail.com>
2025-10-22 15:13:04 -0400

    more stuff

--- src/dfa.c
+++ src/dfa.c
@@ -1,4 +1,3 @@
 
 
 #include "dfa.h"
-
--- src/dfa.h
+++ src/dfa.h
@@ -1,20 +1,24 @@
 
 #include "./def.h"
 
+
 struct dfa {
 	
 	struct state *states;
-	int num_states;
-	
 	char *alphabet;
+	
+	int num_states;
 	int num_symbols;
 	
 	struct transition_function *transition_set;
 	
-	char *start;
-	char **end;
-	int num_ends;
+	/* end state info instead stored directly in state struct */
+	struct state *start; // TODO: determine if its faster to include this extra memory here or just go through states to check for start
+	//char **end;
+	//int num_ends;
 	
 };
 
 int create_dfa(char *state_list, char *alphabet_list, char *transition_list, char *start_state, char *end_state_list);
+
+int progress_dfa(struct dfa *dfa, struct state *current, char *input, int input_loc);
