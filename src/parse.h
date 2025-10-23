e55901b6d3571855db2d3202fdc315e6999d8074
Ava ginsberg <avery@mac.lan>
2025-10-23 17:19:47 -0400

    I forgot to save the header file

--- src/parse.h
+++ src/parse.h
@@ -1,11 +1,18 @@
 #include "def.h"
+#include "dfa.h"
 
 #ifndef PARSE
 #define PARSE
 
+/*
+    Take in inputs in format q1,a,q2;q1,b,q3
+*/
 
-
-
+int parse_fa_states(struct dfa *dfa , char *state_list);
+int parse_fa_alphabet(struct dfa *dfa , char *alphabet_list);
+int parse_fa_transitions(struct dfa *dfa , char *transition_list);
+int parse_fa_start(struct dfa *dfa , char *start_state);
+int parse_fa_end(struct dfa *dfa , char *end_state_list);
 
 
 #endif /* PARSE */
