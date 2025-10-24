94b381540a80b7758a2728c5b9973e576f861604
loganmca01 <loganmca01@gmail.com>
2025-10-24 18:42:07 -0400

    testing

--- src/automata.c
+++ src/automata.c
@@ -9,21 +9,25 @@ int main() {
 	
 	printf("test\n");
 	
-	char *states = "s1,s2,s3";
+	char *states = "s1,s2";
 	char *alphabet = "0,1";
-	char *transitions = "yeah idk";
+	char *transitions = "s1,0,s2;s1,1,s1;s2,0,s1;s2,1,s2";
 	char *start = "s1";
-	char *end = "s3";
+	char *end = "s2";
 	
 	create_dfa(states, alphabet, transitions, start, end);
 	
-	char *input_str = "01101";
+	char *input_str = "0110100010";
 	char *converted_input;
 	initialize_dfa_sequence(input_str, &converted_input);
 	
-	while(progress_dfa_sequence(converted_input) == 0) {
-		printf("something like this");
+	int status;
+
+	while((status = progress_dfa_sequence(converted_input)) == 0) {
+		printf("ongoing\n");
 	}
+
+	printf("%d\n", status);
 	
 	free_dfa_mem(&converted_input);
 	
